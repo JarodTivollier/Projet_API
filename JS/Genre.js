2599847c19e75ffa2a49b59105eb4be4c09c1b0c
tivollij <jarod.tivollier@etu.univ-grenoble-alpes.fr>
2025-03-20 09:02:20 +0100

    template php de la page d'accueil

---
+++ JS/Genre.js
@@ -0,0 +1,1 @@
+const listGenres = document.getElementById("genres");
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="Genre.js"></script>
+</head>
+<body>
+    <div id="header">
+        <h1>Recherche de film avec l'api SMBD </h1>
+        <button id="searchBtn">Rechercher</button>
+    </div>
+    <div id="filmsEtListe">
+        <aside id="listeFilms">
+
+        </aside>
+        <div id="films">
+
+        </div>
+    </div>
+</body>
+</html>
