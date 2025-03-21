f548bf0b1de0910cae686a3d41266cf55ff6cb13
tivollij <jarod.tivollier@etu.univ-grenoble-alpes.fr>
2025-03-21 16:40:27 +0100

    recherche.js Genres fonctionnel

--- js/recherche.js
+++ js/recherche.js
@@ -1,10 +1,17 @@
 import { view } from './view.js';
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjM0Yjc1MTQ4MzFjZGI5ODA3MTNiM2MyNmE1OWY1YiIsIm5iZiI6MTc0MTk2Mjc5Mi4zNzIsInN1YiI6IjY3ZDQzZTI4ZWM0ZWJkMjhhMjUzYzQ5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VXfTdMN68v_vGud1nkMQ8fNqw9ZNnL7-OnbaE7GJr2A'
+    }
+  };
 
 
 
-
-async function AfficheGenre(){
+async function afficheGenre(){
     const genres = await recupGenres();
+    console.log("bonjour");
     for(let i = 0; i < genres.length; i++){
         let genre = document.createElement('option');
         genre.value = genres[i].id;
@@ -17,7 +24,7 @@ async function AfficheGenre(){
     if (resObj.ok) {
       // Si le résultat semble valide (Statut HTTP entre 200 et 299)
       const resJSON = await resObj.json();
-      const genres = resJSON.results;
+      const genres = resJSON.genres;
       // console.log(movies);
       return genres;
     }  else {
@@ -25,49 +32,75 @@ async function AfficheGenre(){
       console.error(resObj);
     }
   }
-let genreText = '';
-view.genres.addEventListener('click', function() {
-    const genre = document.getElementById('select-genre');
-     genreText = genre.options[genre.selectedIndex].text;
-        
-})
+  let genreId = '';  
+  view.listGenres.addEventListener('change', function () {
+      const genre = document.getElementById('genres'); 
+      genreId = view.listGenres.options[genre.selectedIndex].value;  
+      console.log('Genre sélectionné:', genreId);  
+  });
 
+
+function getDuree() {
+    // Récupérer le bouton radio sélectionné
+    const dureeSelectionner = document.querySelector('input[name="runtime"]:checked');
+
+    if (dureeSelectionner) {
+        // Récupérer la valeur (par exemple : "1-1.5" 1.5 = 1h30)
+        const valeur = dureeSelectionner.value;
+
+        // Si l'intervalle est 'plus de 3h'
+        if (valeur === '>3') {
+            return { start: 180, end: Infinity }; // 180 minutes  à l'infini
+        }
+
+        // Séparer les heures et minutes pour l'intervalle
+        const [start, end] = valeur.split('-').map(value => {
+            const [hours, minutes] = value.split('h').map(Number);
+            return (hours * 60) + minutes; // Converti en minutes
+        });
+
+        return { start, end };
+    } else {
+        console.log("Aucune durée sélectionné.");
+        return null;
+    }
+}
+
+
+  
 
 async function recherche(){
 
-    const titre = view.titre.value || '';
-    const genre = genreText || '';
-    const date = view.date.value || '';
-    const dureeMin = view.dureeMin.value || '';
-    const dureeMax = view.dureeMax.value || '';
+    const genre = genreId || '';
+    const dateMin = view.dateMin.value || '';
+    const dateMax = view.dateMax.value || '';
+    const keywords = view.keywords.value || '';
+    const duree = getDuree();
     const movies = [];
-
-    if (titre != ''){
-        let resObj = await fetch('https://api.themoviedb.org/3/search/movie?query='+titre+'&include_adult=false&language=en-US&page=1', options)
-        if (resObj.ok) {
-            // Si le résultat semble valide (Statut HTTP entre 200 et 299)
-            const resJSON = await resObj.json();
-            movies = resJSON.results;
-        }  else {
-            // Sinon on affiche l'erreur
-            console.error(resObj);
+    const moviesId = [];
+    let resObj = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte='+dateMin+'&primary_release_date.lte='+dateMax+'&sort_by=popularity.desc&with_genres='+genre+'&with_keywords='+keywords+'&with_runtime.gte='+duree.start+'&with_runtime.lte='+duree.end+'', options)
+    if (resObj.ok) {
+        // Si le résultat semble valide (Statut HTTP entre 200 et 299)
+        const resJSON = await resObj.json();
+        movies = resJSON.results;
+        movies = JSON.stringify(movies);
+        for (let i = 0; i < 20; i++){
+            moviesId = movies.id;
         }
-    }else {
-        let resObj = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year='+date+'&sort_by=popularity.desc&with_genres='+genre+'&with_runtime.gte='+dureeMin+'&with_runtime.lte='+dureeMax+'', options)
-        if (resObj.ok) {
-            // Si le résultat semble valide (Statut HTTP entre 200 et 299)
-            const resJSON = await resObj.json();
-            movies = resJSON.results;
-        }  else {
-            // Sinon on affiche l'erreur
-            console.error(resObj);
-        }
+    }  else {
+        // Sinon on affiche l'erreur
+        console.error(resObj);
     }
-    // Stocker les films dans le localStorage
-    localStorage.setItem('movies', JSON.stringify(movies));
-    
+        
+    // Stocker les données dans l'objet window
+    localStorage.setItem('searchMoviesId', JSON.stringify(moviesId));
     // Rediriger vers index.html
     window.location.href = 'index.html';
 }
 
-view.recherche.addEventListener()
+view.recherche.addEventListener('click', (event) => {
+    recherche();
+});
+document.addEventListener('DOMContentLoaded', () => {
+    afficheGenre();  
+});
