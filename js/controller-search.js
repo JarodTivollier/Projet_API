45989cb163d5e25f26df74dffba6887fb9c744b7
dubiefe <emilie.dubief@etu.univ-grenoble-alpes.fr>
2025-03-23 11:28:42 +0100

    Réparation de l'url 3

--- js/controller-search.js
+++ js/controller-search.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         console.log('bonjour');
         await search();
         window.history.back();
+        window.location.reload();
     });
 });
  
