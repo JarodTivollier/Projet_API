ce7dbf357e5fa6ed71ef19adc72f15a5419039c4
dubiefe <emilie.dubief@etu.univ-grenoble-alpes.fr>
2025-03-26 19:56:20 +0100

    Réparation de l'url

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ import { Controleur } from "./controller/controller.js";
 import { ControllerSearch } from "./controller/controller-search.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Vérifier si on est sur la page d'accueil
-    if (window.location.pathname.includes("index.html")) {
-      const controller = new Controleur();
-      controller.initialize();
-    }
-    
-    // Vérifier si on est sur la page de recherche
+    // Check if we are on the search page
     if (window.location.pathname.includes("search.html")) {
         const controllerSearch = new ControllerSearch();
         controllerSearch.initialize();
+    } else { // We are on the index
+      const controller = new Controleur();
+      controller.initialize();
     }
   });
